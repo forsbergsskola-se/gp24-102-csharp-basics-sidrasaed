a40462cb8d2c620ac72916b433102e3271386ed5
Sidra Saeed <saeedsidra004@gmail.com>
2024-09-17 11:08:33 +0200

    Exercises 8 - Conversion:

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Give me a number.");
+string userInput = Console.ReadLine();
+float number = float.Parse(userInput);
+Console.WriteLine(number);
+int userInputInt = (int)number;
+Console.WriteLine(userInputInt);
+
+
