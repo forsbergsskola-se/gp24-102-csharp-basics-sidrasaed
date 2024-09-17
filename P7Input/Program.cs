30bc1b70417d06670331e0f544a6163f22fb5466
Sidra Saeed <saeedsidra004@gmail.com>
2024-09-17 10:16:14 +0200

    Exercises 7 - Reading Input:

---
+++ P7Input/Program.cs
@@ -0,0 +1,9 @@
+﻿/*string a = "What's your name?";
+Console.WriteLine(a);
+string Name = "Sidra!";
+string response =$"Hello {Name}";
+Console.WriteLine(response);*/
+
+Console.WriteLine("What's your name?");
+string inputstring = Console.ReadLine();
+Console.WriteLine($"Hello! {inputstring}");
